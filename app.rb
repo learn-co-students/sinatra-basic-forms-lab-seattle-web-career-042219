f88680fb65149fc7479ff576279efc32cb9c4d05
Milan <milan.dhar@gmail.com>
2019-05-14 16:40:47 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @months_old = params[:age]
+    erb :display_puppy
+  end
 
 end
